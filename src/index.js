d85dfc6c129aa034bad22c64ae4e8808b4c876f6
Dhafer Alzubairi <dx.dhafer@gmail.com>
2023-08-07 21:41:10 +0200

    added the new uri

--- src/index.js
+++ src/index.js
@@ -11,25 +11,26 @@ import Category from './pages/Category/Category.Component';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'https://dhafer-e-commerce-react-app.herokuapp.com',
-	cache: new InMemoryCache(),
+    uri: 'https://git.heroku.com/dx-ecommerce.git',
+    // uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<ApolloProvider client={client}>
-				<Routes>
-					<Route path='/' element={<App client={client} />}>
-						<Route path='/plp/:plp' element={<Category client={client} />} />
-						<Route path='/plp/:plp/:pdp' element={<Product client={client} />} />
-						<Route path='/cart' element={<Cart client={client} />} />
-					</Route>
-				</Routes>
-			</ApolloProvider>
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root'),
+    <React.StrictMode>
+        <Router>
+            <ApolloProvider client={client}>
+                <Routes>
+                    <Route path='/' element={<App client={client} />}>
+                        <Route path='/plp/:plp' element={<Category client={client} />} />
+                        <Route path='/plp/:plp/:pdp' element={<Product client={client} />} />
+                        <Route path='/cart' element={<Cart client={client} />} />
+                    </Route>
+                </Routes>
+            </ApolloProvider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
